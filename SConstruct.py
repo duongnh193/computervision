150dbae57d06652e79bf12a078dd8c6977d99c13
21013110DuongNH <21013110@st.phenikaa-uni.edu.vn>
2024-07-26 11:37:59 +0700

    Modified SConstruct

--- SConstruct.py
+++ SConstruct.py
@@ -2,7 +2,9 @@
 import os
 import sys
 
+
 env = SConscript("godot-cpp/SConstruct")
+
 
 # For reference:
 # - CCFLAGS are compilation flags shared between C and C++
@@ -13,8 +15,8 @@ env = SConscript("godot-cpp/SConstruct")
 # - LINKFLAGS are for linking flags
 
 # tweak this if you want to use different folders, or more folders, to store your source code in.
-env.Append(CPPPATH=["src/"])
-sources = Glob("src/*.cpp")
+env.Append(CPPPATH=["FaceDetectionGodot/"])
+sources = Glob("FaceDetectionGodot/*.cpp")
 
 if env["platform"] == "macos":
     library = env.SharedLibrary(
@@ -28,5 +30,19 @@ else:
         "demoproject/bin/FaceDetect{}{}".format(env["suffix"], env["SHLIBSUFFIX"]),
         source=sources,
     )
+opencv_include = os.popen("pkg-config --cflags opencv4").read().split()
+opencv_libs = os.popen("pkg-config --libs opencv4").read().split()
+
+env.Append(CPPPATH=[p[2:] for p in opencv_include])
+env.Append(LIBPATH=[opencv_libs[0][2:]])
+env.Append(LIBS=[p[2:] for p in opencv_libs[1:]])
+
+tensorflow_include = os.popen("pkg-config --cflags tensorflowlite").read().split()
+tensorflow_libs = os.popen("pkg-config --libs tensorflowlite").read().split()
+
+
+env.Append(CPPPATH=[p[2:] for p in tensorflow_include])
+env.Append(LIBPATH=[tensorflow_libs[0][2:]])
+env.Append(LIBS=[p[2:] for p in tensorflow_libs[1:]])
 
 Default(library)
